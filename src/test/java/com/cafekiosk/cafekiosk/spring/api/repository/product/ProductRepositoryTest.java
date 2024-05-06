75b68085d73c5a3e3008c4db790d85d7e6e1912b
jaeyoungjang2 <lion85488@gmail.com>
2024-05-06 14:28:08 +0900

    feat: Presentation Layer 테스트 (1)  - 상품 번호 생성 로직에 대한 테스트 (findLatestProductNumber)  - 예외에 대한 테스트

--- src/test/java/com/cafekiosk/cafekiosk/spring/api/repository/product/ProductRepositoryTest.java
+++ src/test/java/com/cafekiosk/cafekiosk/spring/api/repository/product/ProductRepositoryTest.java
@@ -98,6 +98,16 @@ class ProductRepositoryTest {
         assertThat(latestProductNumber).isEqualTo(targetProductNumber);
     }
 
+    @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어올 때, 상품이 하나도 없는 경우에는 null을 반환한다.")
+    @Test
+    void findLatestProductNumberNumberWhenProductIsEmpty() {
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(latestProductNumber).isNull();
+    }
+
     private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         return Product.builder()
                 .productNumber(productNumber)
